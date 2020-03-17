1ef864d2910054d044b251dd95609ab55f4d89f9
Patrick Kille <pr.kille@gmail.com>
2020-03-17 09:35:06 -0700

    refactor for LINQ and Entity.
    Co-authored-by: Patrick Delaney <prestwick97@gmail.com>
    Co-authored-by: Joseph Wangemann <fractalscape13@gmail.com>

---
+++ ToDoList/Models/Item.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using MySql.Data.MySqlClient;
+
+namespace ToDoList.Models
+{
+  public class Item
+  {
+    public string Description { get; set; }
+    public int ItemId { get; set; }
+  }
+}
